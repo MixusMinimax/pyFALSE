6074b1c27054034fbf876ff44663b820d2c8f23f
Maxi <maxi.barmetler@gmail.com>
2021-11-19 01:09:06 +0100

    refactor example

--- examples/add.f
+++ examples/add.f
@@ -1,8 +1,6 @@
 { read until you see \n or \r, and convert decimal to number: }
 [0[ß^$$10=~@13=~&]['0-\10*+]#%]n:
-
-"Add two numbers together!
-A: "n;!$."
-B: "n;!$.+"
-Result: "."
+"A: "n;!$$a:."
+B: "n;!$$b:.+"
+"a;." + "b;." = "."
 "
